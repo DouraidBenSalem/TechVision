a36478ad86e51ec81c7da5d004ee4cda4083889f
DouraidBenSalem <douraid.bensalem@esprit.tn>
2024-02-16 22:37:40 +0100

    prjt

---
+++ projet/file1.h
@@ -0,0 +1,27 @@
+#ifndef file1_H
+#define file1_H
+
+#include <QMainWindow>
+#include <QButtonGroup>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class file1; }
+QT_END_NAMESPACE
+
+class file1 : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    file1(QWidget *parent = nullptr);
+    ~file1();
+
+private slots:
+    void selectSecondTab();
+
+private:
+    Ui::file1 *ui;
+};
+
+
+#endif // file1_H
